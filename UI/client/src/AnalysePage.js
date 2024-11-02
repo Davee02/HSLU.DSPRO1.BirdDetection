09a10f756209ec859b6c1e7b713046e3b58d3f76
gnu <41645782+NevinJulian@users.noreply.github.com>
2024-11-02 14:23:08 +0100

    add starting location map

--- UI/client/src/AnalysePage.js
+++ UI/client/src/AnalysePage.js
@@ -1,10 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import { MapContainer, TileLayer, Marker, useMap } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker } from 'react-leaflet';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import './AnalysePage.css';
 
-// Fixes default icon issue in Leaflet
 delete L.Icon.Default.prototype._getIconUrl;
 L.Icon.Default.mergeOptions({
   iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
@@ -17,7 +16,7 @@ const AnalysePage = () => {
   const [description, setDescription] = useState(
     'Reminder: Make sure your recording is between 2 and 15 seconds, has good sound quality, and is from the European region.'
   );
-  const [position, setPosition] = useState([51.505, -0.09]); // Default position in London
+  const [position, setPosition] = useState(null);
 
   useEffect(() => {
     if (navigator.geolocation) {
@@ -85,17 +84,21 @@ const AnalysePage = () => {
       </div>
 
       <div className="map-box">
-        <MapContainer
-          center={position}
-          zoom={13}
-          style={{ height: '100%', width: '100%' }}
-        >
-          <TileLayer
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          />
-          <Marker position={position}></Marker>
-        </MapContainer>
+        {position ? (
+          <MapContainer
+            center={position}
+            zoom={13}
+            style={{ height: '100%', width: '100%' }}
+          >
+            <TileLayer
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            />
+            <Marker position={position}></Marker>
+          </MapContainer>
+        ) : (
+          <p>Loading map...</p>
+        )}
       </div>
     </div>
   );
