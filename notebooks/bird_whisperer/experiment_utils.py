d731076646cf247f0e5196574033e2b49e8de611
David Hodel <davidhodel6@gmail.com>
2024-12-07 13:14:36 +0100

    parse command line arguments

--- notebooks/bird_whisperer/experiment_utils.py
+++ notebooks/bird_whisperer/experiment_utils.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 import torch
 
@@ -14,3 +15,17 @@ def load_from_checkpoint(checkpoint_path, model, optimizer):
     best_epoch = checkpoint["best_epoch"]
 
     return model, optimizer, epoch, epoch_avg_loss, best_f1_score, best_epoch
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Bird Whisperer Trainer")
+
+    parser.add_argument("--seed", type=int, default=42, help="Seed for reproducibility (default: 42)")
+    parser.add_argument("--lr", type=float, default=3e-4, help="Learning rate (default: 3e-4)")
+    parser.add_argument("--epochs", type=int, default=10, help="Number of epochs (default: 10)")
+    parser.add_argument("--with_augmented", action="store_true", default=True, help="Use augmented data (default: True)")
+    parser.add_argument("--whisper_base_variant", type=str, default="tiny", help="Whisper model base variant (default: tiny)")
+    parser.add_argument("--checkpoint_file", type=str, default=None, help="Checkpoint file to load (default: None)")
+    parser.add_argument("--debug", action="store_true", default=False, help="Debug mode (default: False)")
+    parser.add_argument("--batch_size", type=int, default=16, help="Batch size (default: 16)")
+
+    return parser.parse_args()
--- notebooks/bird_whisperer/main.py
+++ notebooks/bird_whisperer/main.py
@@ -7,15 +7,7 @@ from trainer import train
 from logging_utils import setup_logging
 from datasets.dataset_utils import get_dataloaders
 from whisper_model import whisper_model
-from experiment_utils import load_from_checkpoint
-
-LR = 3e-4
-SEED = 42
-EPOCHS = 10
-WITH_AUGMENTED = True
-WHISPER_BASE_VARIANT = "tiny"
-CHECKPOINT = "checkpoint_epoch_1_loss_5.5116.pt"
-DEBUG = False
+from experiment_utils import load_from_checkpoint, parse_arguments
 
 def set_seed(seed):
   print(f"Setting seed: {seed}")
@@ -25,7 +17,7 @@ def set_seed(seed):
   np.random.seed(seed)
   random.seed(seed)
 
-def print_dataset_info(train_dataloader, test_dataloader):
+def print_dataset_info(train_dataloader, test_dataloader, args):
     num_batches_train = len(train_dataloader)
     num_batches_test = len(test_dataloader)
 
@@ -38,39 +30,47 @@ def print_dataset_info(train_dataloader, test_dataloader):
     print("Number of Batches in Test Dataloader: ", num_batches_test)
     print("Test Batch Size: ", test_dataloader.batch_size)
     print()
-    print(f"Use augmented data: {WITH_AUGMENTED}\n\n")
+    print(f"Use augmented data: {args.with_augmented}\n\n")
 
-def main(json_log_file_path):
+def main(json_log_file_path, args):
+  print("\n================================= Bird Whisperer Trainer =================================")
+  print(f"Arguments: {args}\n")
+
   device = torch.device("cuda" if torch.cuda.is_available() else "cpu") # use GPU if available for torch, else use CPU
   print(f"Using device: {device}")
   if device.type == "cpu":
     raise Exception("This script is not optimized for CPU. It is recommended to use a GPU.")
 
-  set_seed(SEED) # set seed for repro
+  set_seed(args.seed) # set seed for repro
 
   dataset_root = os.path.join(os.path.dirname(__file__), "../../data/processed/bird-whisperer") # define path to dataset
-  train_dataloader, test_dataloader, unique_labels = get_dataloaders(dataset_root, with_augmented=WITH_AUGMENTED)
-  print_dataset_info(train_dataloader, test_dataloader)
+  train_dataloader, test_dataloader, unique_labels = get_dataloaders(dataset_root, with_augmented=args.with_augmented, batch_size=args.batch_size)
+  print_dataset_info(train_dataloader, test_dataloader, args)
 
   save_model_path = os.path.join(os.path.dirname(__file__), "../../data/bird-whisperer/models") # define path to where the model will be saved
   print(f"Saving models to: {save_model_path}")
 
   models_save_dir = os.path.join(save_model_path, "trained")
 
-  model = whisper_model.WhisperModel(n_classes=len(unique_labels), models_root_dir=save_model_path, variant=WHISPER_BASE_VARIANT, device=device) 
-  optimizer = torch.optim.AdamW(model.parameters(), lr=LR, amsgrad=True)
-  start_epoch = 0
+  model = whisper_model.WhisperModel(n_classes=len(unique_labels), models_root_dir=save_model_path, variant=args.whisper_base_variant, device=device) 
+  optimizer = torch.optim.AdamW(model.parameters(), lr=args.lr, amsgrad=True)
 
-  if CHECKPOINT:
-    checkpoint_path = os.path.join(models_save_dir, CHECKPOINT)
+  start_epoch = 0
+  best_epoch = 0
+  best_f1_score = 0
+
+  checkpoint_file = args.checkpoint_file
+  if checkpoint_file:
+    checkpoint_path = os.path.join(models_save_dir, checkpoint_file)
     print(f"Loading model from checkpoint: {checkpoint_path}")
     model, optimizer, start_epoch, _, best_f1_score, best_epoch = load_from_checkpoint(checkpoint_path, model, optimizer)
 
   model = model.to(device) # move model to device (GPU or CPU)
   criterion = torch.nn.CrossEntropyLoss() # loss function
 
-  train(device, model, train_dataloader, test_dataloader, criterion, optimizer, unique_labels, start_epoch, EPOCHS, models_save_dir, json_log_file_path, best_f1_score=best_f1_score, best_epoch=best_epoch, debug=DEBUG)
+  train(device, model, train_dataloader, test_dataloader, criterion, optimizer, unique_labels, start_epoch, args.epochs, models_save_dir, json_log_file_path, best_f1_score=best_f1_score, best_epoch=best_epoch, debug=args.debug)
 
 if __name__ == "__main__":
+  args = parse_arguments()
   _, json_log_file_path = setup_logging()
-  main(json_log_file_path)
+  main(json_log_file_path, args)
