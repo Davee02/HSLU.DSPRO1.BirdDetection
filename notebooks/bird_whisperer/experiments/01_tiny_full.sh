c02561ad3ada06f332fbca32554655abe8ecdecd
David Hodel <davidhodel6@gmail.com>
2024-12-08 18:56:06 +0100

    add weight decay for AdamW as argument

--- notebooks/bird_whisperer/experiments/01_tiny_full.sh
+++ notebooks/bird_whisperer/experiments/01_tiny_full.sh
@@ -5,4 +5,5 @@ python ../main.py --seed 42 \
   --epochs 20 \
   --with_augmented \
   --whisper_base_variant tiny \
-  --batch_size 16
+  --batch_size 16 \
+  --weight_decay 1e-2
--- notebooks/bird_whisperer/experiments/02_tiny_no_augmented.sh
+++ notebooks/bird_whisperer/experiments/02_tiny_no_augmented.sh
@@ -4,4 +4,5 @@ python ../main.py --seed 42 \
   --lr 3e-4 \
   --epochs 20 \
   --whisper_base_variant tiny \
-  --batch_size 16
+  --batch_size 16 \
+  --weight_decay 1e-2
