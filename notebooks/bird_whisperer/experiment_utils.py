d7f1823e95c693711569ffe8ddad9b3943a452ab
David Hodel <davidhodel6@gmail.com>
2024-12-16 11:00:43 +0100

    add dataset root and parquet file names as arguments

--- notebooks/bird_whisperer/experiment_utils.py
+++ notebooks/bird_whisperer/experiment_utils.py
@@ -22,6 +22,12 @@ def load_from_checkpoint(checkpoint_path, model, optimizer, map_location):
 
     return model, optimizer, epoch, epoch_avg_loss, best_f1_score, best_epoch
 
+def dir_path(path):
+    if os.path.isdir(path):
+        return path
+    else:
+        raise argparse.ArgumentTypeError(f"readable_dir:{path} is not a valid path")
+
 def parse_arguments():
     parser = argparse.ArgumentParser(description="Bird Whisperer Trainer")
 
@@ -34,5 +40,8 @@ def parse_arguments():
     parser.add_argument("--debug", action="store_true", default=False, help="Debug mode (default: False)")
     parser.add_argument("--batch_size", type=int, default=16, help="Batch size (default: 16)")
     parser.add_argument("--weight_decay", type=float, default=1e-2, help="Weight decay for AdamW optimizer (default: 1e-2)")
+    parser.add_argument("--train_parquet_name", type=str, default="train.parquet", help="File name of the train parquet file (default: train.parquet)")
+    parser.add_argument("--test_parquet_name", type=str, default="test.parquet", help="File name of the test parquet file (default: test.parquet)")
+    parser.add_argument("--dataset_root", type=dir_path, default=os.path.join(os.path.dirname(__file__), "../../data/processed/bird-whisperer"), help="Path to dataset root (default: ../../data/processed/bird-whisperer)")
 
     return parser.parse_args()
