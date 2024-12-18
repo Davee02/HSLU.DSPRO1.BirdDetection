6eb62a97954323bc5901e5b022edc11d0fb17710
David Hodel <davidhodel6@gmail.com>
2024-12-18 16:10:20 +0100

    add additional experiment with all species

---
+++ notebooks/bird_whisperer/experiments/05_tiny_all_species.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+python ../main.py --seed 42 \
+  --lr 3e-4 \
+  --epochs 20 \
+  --with_augmented \
+  --whisper_base_variant tiny \
+  --batch_size 24 \
+  --weight_decay 1e-2 \
+  --dropout_p 0.5 \
+  --dataset_root /exchange/dspro01/HSLU.DSPRO1.BirdDetection/data/processed/bird-whisperer-denoised \
+  --train_parquet_name train.parquet \
+  --test_parquet_name test.parquet
