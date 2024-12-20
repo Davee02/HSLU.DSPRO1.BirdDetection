d57641a880ccbb8050a2388c6bdbb050bd33017a
gnu <41645782+NevinJulian@users.noreply.github.com>
2024-12-20 16:35:29 +0100

    add the new experiment execution files

---
+++ notebooks/cnn_baseline/experiments/02_without_augmented.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+python ../main.py --seed 42 \
+    --lr 3e-4 \
+    --epochs 20 \
+    --batch_size 24 \
+    --weight_decay 1e-2 \
+    --dropout_p 0.5 \
+    --dataset_root /exchange/dspro01/HSLU.DSPRO1.BirdDetection/data/processed/bird-whisperer-denoised \
+    --train_parquet_name train_cutoff.parquet \
+    --test_parquet_name test_cutoff.parquet
