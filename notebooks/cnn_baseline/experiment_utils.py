6601ab2429c2659c2be1ea01c259228119f4e69b
gnu <41645782+NevinJulian@users.noreply.github.com>
2024-12-21 17:16:37 +0100

    remove not needed parameter

--- notebooks/cnn_baseline/experiment_utils.py
+++ notebooks/cnn_baseline/experiment_utils.py
@@ -35,7 +35,6 @@ def parse_arguments():
     parser.add_argument("--lr", type=float, default=3e-4, help="Learning rate (default: 3e-4)")
     parser.add_argument("--epochs", type=int, default=10, help="Number of epochs (default: 10)")
     parser.add_argument("--with_augmented", action="store_true", default=False, help="Use augmented data (default: True)")
-    parser.add_argument("--whisper_base_variant", type=str, default="tiny", help="Whisper model base variant (default: tiny)")
     parser.add_argument("--checkpoint_file", type=str, default=None, help="Checkpoint file to load (default: None)")
     parser.add_argument("--debug", action="store_true", default=False, help="Debug mode (default: False)")
     parser.add_argument("--batch_size", type=int, default=16, help="Batch size (default: 16)")
