4ba9c492eda3aa2e2fdf3df61f104fbd40a5c47c
gnu <41645782+NevinJulian@users.noreply.github.com>
2024-12-23 22:55:08 +0100

    fix dataloader

--- notebooks/evaluate_best_model/datasets/dataset_utils.py
+++ notebooks/evaluate_best_model/datasets/dataset_utils.py
@@ -3,34 +3,44 @@ import pandas as pd
 from torch.utils.data import DataLoader
 from .dataset import XenoCantoDataset
 
-def get_dataloaders(dataset_root, batch_size, num_workers, train_parquet_name, test_parquet_name, with_augmented=True):
-    train_parquet_path = os.path.join(dataset_root, train_parquet_name)
-    test_parquet_path = os.path.join(dataset_root, test_parquet_name)
+def get_test_data(test_df, bird2label_dict):
+    lambda_function = lambda bird: bird2label_dict[bird]
 
-    train_df = pd.read_parquet(train_parquet_path)
+    test_df.insert(1, "labels", None) # adding new column "labels" at index=1. All values will be None in this column
+    test_df["labels"] = test_df["species"].apply(lambda_function) # filling the "labels" column with integer labels
+
+    audio_files_paths_test = test_df["spectogram_file"].values.tolist()
+    labels_test = test_df["labels"].values.tolist()
+
+    return audio_files_paths_test, labels_test
+
+def get_test_dataloader(dataset_root, batch_size, num_workers, test_parquet_name, with_augmented=True):
+    test_parquet_path = os.path.join(dataset_root, test_parquet_name)
     test_df = pd.read_parquet(test_parquet_path)
 
     if not with_augmented:
-        train_df = train_df[train_df["augmented"] == False]
         test_df = test_df[test_df["augmented"] == False]
 
-    bird2label_dict, label2bird_dict = get_dicts(pd.concat([train_df, test_df]))
+    bird2label_dict, label2bird_dict = get_dicts(test_df)
 
-    audio_files_paths_test = test_df["spectogram_file"].values.tolist()
-    labels_test = test_df["labels"].map(bird2label_dict).values.tolist()
+    audio_files_paths_test, labels_test = get_test_data(test_df, bird2label_dict)
 
     test_spectograms_folder = os.path.join(dataset_root, "spectograms", "test")
     test_dataset = XenoCantoDataset(audio_files_paths_test, labels_test, test_spectograms_folder, bird2label_dict, label2bird_dict)
 
     test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers)
 
-    return None, test_loader, label2bird_dict
+    return test_loader, label2bird_dict
 
 def get_dicts(df):
     primary_labels = df["species"].values.tolist()
     primary_labels_unique = sorted(list(set(primary_labels)))
 
-    bird2label_dict = {bird: i for i, bird in enumerate(primary_labels_unique)}
-    label2bird_dict = {i: bird for bird, i in bird2label_dict.items()}
+    bird2label_dict = {} # this dictionary will give integer "label" when a "bird" name is used in key
+    label2bird_dict = {} # this dictionary will give "bird" name when integer "label" is used in key
 
+    for i, bird in enumerate(primary_labels_unique):
+        bird2label_dict[bird] = i
+        label2bird_dict[i] = bird
+    
     return bird2label_dict, label2bird_dict
