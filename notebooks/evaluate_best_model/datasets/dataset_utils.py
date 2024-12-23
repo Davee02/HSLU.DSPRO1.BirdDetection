65cf3e6d4763c534b2d0b3344af5adbe91077d49
gnu <41645782+NevinJulian@users.noreply.github.com>
2024-12-23 22:06:17 +0100

    fix dataloader to only use test set

--- notebooks/evaluate_best_model/datasets/dataset_utils.py
+++ notebooks/evaluate_best_model/datasets/dataset_utils.py
@@ -3,28 +3,21 @@ import pandas as pd
 from torch.utils.data import DataLoader
 from .dataset import XenoCantoDataset
 
-def get_train_test_data(train_df, test_df, bird2label_dict, seed=42):
+def get_train_test_data(test_df, bird2label_dict, seed=42):
     lambda_function = lambda bird: bird2label_dict[bird]
-
-    train_df.insert(1, "labels", None) # adding new column "labels" at index=1. All values will be None in this column
-    train_df["labels"] = train_df["species"].apply(lambda_function) # filling the "labels" column with integer labels
 
     test_df.insert(1, "labels", None) # adding new column "labels" at index=1. All values will be None in this column
     test_df["labels"] = test_df["species"].apply(lambda_function) # filling the "labels" column with integer labels
 
     # shuffling the dataframes
-    train_df = train_df.sample(frac=1, random_state=seed).reset_index(drop=True)
     test_df = test_df.sample(frac=1, random_state=seed).reset_index(drop=True)
-
-    audio_files_paths_train = train_df["spectogram_file"].values.tolist()
-    labels_train = train_df["labels"].values.tolist()
 
     audio_files_paths_test = test_df["spectogram_file"].values.tolist()
     labels_test = test_df["labels"].values.tolist()
 
-    labels_unique = sorted(list(set(labels_train)))
+    labels_unique = sorted(list(set(labels_test)))
 
-    return audio_files_paths_train, labels_train, audio_files_paths_test, labels_test, labels_unique
+    return audio_files_paths_test, labels_test, labels_unique
 
 def get_dataloaders(dataset_root, batch_size, num_workers, test_parquet_name, with_augmented=True):
     test_parquet_path = os.path.join(dataset_root, test_parquet_name)
@@ -38,17 +31,14 @@ def get_dataloaders(dataset_root, batch_size, num_workers, test_parquet_name, wi
 
     assert len(bird2label_dict) == len(label2bird_dict), "Bird2Label and Label2Bird dictionaries are not equal"
 
-    audio_files_paths_train, labels_train, audio_files_paths_test, labels_test, labels_unique = get_train_test_data(test_df, bird2label_dict)
+    audio_files_paths_test, labels_test, labels_unique = get_train_test_data(test_df, bird2label_dict)
 
-    train_spectograms_folder = os.path.join(dataset_root, "spectograms", "train")
     test_spectograms_folder = os.path.join(dataset_root, "spectograms", "test")
-    train_dataset = XenoCantoDataset(audio_files_paths_train, labels_train, train_spectograms_folder, bird2label_dict, label2bird_dict)
     test_dataset = XenoCantoDataset(audio_files_paths_test, labels_test, test_spectograms_folder, bird2label_dict, label2bird_dict)
 
-    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers)
     test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers)
 
-    return train_loader, test_loader, labels_unique
+    return test_loader, labels_unique
 
 def get_dicts(df):
     primary_labels = df["species"].values.tolist()
